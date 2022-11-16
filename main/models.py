0c561b39a56fd434b4efe76bae163b0bab07afe5
dmitriy <dmitriyvasil@gmail.com>
2022-11-16 20:46:29 +0100

    update password class add field class

--- main/models.py
+++ main/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 
+class Field(models.Model):
+    value = models.BinaryField(max_length=150)
+    name = models.CharField(max_length=150)
+
+    password = models.ForeignKey(
+        on_delete=models.CASCADE,
+        related_name="fields",
+        to="Passwords",
+    )
+
+
 class Passwords(models.Model):
     description = models.CharField(max_length=150, blank=True, null=True)
     name = models.CharField(max_length=150, unique=True)
-    password = models.BinaryField(max_length=150)
 
     def json(self):
-        payload = {
+        return {
             "id": self.id,
             "name": self.name,
-            "password": self.password,
-            "description": self.description,
+            "fields": {x.name: x.value for x in self.fields.all()},
         }
-        return payload
